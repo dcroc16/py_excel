8ce7bd4d6e8b60a248e2d8f598d6f1ca9a4b24cc
David Crocker <davidwcrocker@gmail.com>
2017-08-06 11:29:47 -0500

    working on formats in openpyxl

--- app.py
+++ app.py
@@ -1,4 +1,44 @@
 import openpyxl as xl
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+ft = Font(name='Calibri',
+                 size=12,
+                bold=True,
+                 italic=False,
+                 vertAlign=None,
+                 underline='none',
+                 strike=False,
+                 color='FF000000')
+
+fill = PatternFill(fill_type=None,
+                 start_color='FFFFFFFF',
+                 end_color='FF000000')
+border = Border(left=Side(border_style=None,
+                           color='FF000000'),
+                 right=Side(border_style=None,
+                            color='FF000000'),
+                 top=Side(border_style=None,
+                          color='FF000000'),
+                 bottom=Side(border_style=None,
+                             color='FF000000'),
+                 diagonal=Side(border_style=None,
+                               color='FF000000'),
+                 diagonal_direction=0,
+                 outline=Side(border_style=None,
+                              color='FF000000'),
+                 vertical=Side(border_style=None,
+                               color='FF000000'),
+                 horizontal=Side(border_style=None,
+                                color='FF000000')
+                )
+alignment=Alignment(horizontal='general',
+                     vertical='bottom',
+                     text_rotation=0,
+                     wrap_text=False,
+                     shrink_to_fit=False,
+                     indent=0)
+number_format = 'General'
+protection = Protection(locked=True,
+                         hidden=False)
 
 print("hello world")
 
@@ -13,6 +53,15 @@ i = 1
 
 while sheet.cell(row=i, column=1).value != None:
     print(sheet.cell(row=i, column=1).value, sheet.cell(row=i, column=2).value)
+    sheet.cell(row=i, column=3).value = "=5*" + str(i)
+    sheet.cell(row=i, column=3).font = ft
     i += 1
 
+
+print(sheet.max_row, sheet.max_column)
+
+
+sheet["A25"].value = "1025"
+
+wb.save("example.xlsx")
 
